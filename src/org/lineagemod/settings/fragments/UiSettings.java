be6fe1afb709c597c4295ae1da2661ad3d3f189c
Beta Soft <betaxab@gmail.com>
2021-06-23 21:32:27 +0800

    Convert to LOS MOD
    Signed-off-by: Beta Soft <betaxab@gmail.com>

---
+++ src/org/lineagemod/settings/fragments/UiSettings.java
@@ -0,0 +1,55 @@
+package org.lineagemod.settings.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class UiSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.mod_settings_ui);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+
+        return false;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.SCREEN_OFF_ANIMATION, 0, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.MOD_SETTINGS;
+    }
+}
